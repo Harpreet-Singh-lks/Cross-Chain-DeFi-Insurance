13e461e95923501839060bf9fcab089f8ba81093
Harpreet-Singh-lks <harpreet_s1@ee.iitr.ac.in>
2025-05-18 23:48:12 +0530

    contracts and initial file

---
+++ Contracts/policyManager.sol
@@ -0,0 +1,14 @@
+//SPDX License Identifier- MIT
+pragma solidity ^0.8.0;
+
+contract policyManager{
+    struct Policy{
+        uint256 policy_ID;
+        address asset_addrress;
+        address user_address;
+        
+        
+
+
+    }
+}
